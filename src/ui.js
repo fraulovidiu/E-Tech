dc4a542cc5623eb059ae52fd4d36199da3923143
Filip Raul Ovidiu <andrei.popescu@gmail.com>
2021-04-16 04:46:18 +0300

    Better version cartth CSS but without cart

--- src/ui.js
+++ src/ui.js
@@ -5,6 +5,7 @@ class  UI {
         this.title = document.getElementById('title');
         this.price = document.getElementById('price');
         this.description = document.getElementById('description');
+        this.memory = document.getElementById('memory')
         this.detailsDiv = document.getElementById('product-details');
         this.adminDiv = document.getElementById('admin-control');
         this.id = document.getElementById('id');
@@ -21,9 +22,8 @@ class  UI {
             <div class="card m-3" style="width: 18rem;">
             <div class="card-body">
             <img src="${product.image}" class="card-img-top" alt"...">
-            <h4 class="card-title">${product.title} <br> <span> ${product.price} EUR </span></h4>
+            <h4 class="card-title"><span id="title">${product.title}</span><span id="price"> ${product.price} EUR </span></h4>
             <button onclick="window.location.href='details.html?id=${product.id}'" type="button" class="btn btn-dark">Details</button>
-
             </div>
             </div>
             `;
@@ -36,13 +36,21 @@ class  UI {
 
             output = `
             <div class="details-card">
-                <h4 class="card-title1">${product.title}</h4>
-                <img src="${product.image}" class="card-img1">
-                <h5 class="card-price1">${product.price}</h5>
-                <h6 class="card-description">${product.description}</h6>
-                <input class="quantity" type="number" min="1" max="10">
-                <button  id="addProductToCart" type="button" class="card-button">Add to Cart</button>
+                <div class="row">
+                    <div class="col-md-5">
+                        <img src="${product.image}" class="card-img1">
+                    </div>
+                    <div class="col-md-7">
+                        <h4 class="d-flex justify-content-center">${product.title}</h4>
+                        <h6 class="card-description">${product.description}</h6>
+                        <h5 class="card-price1 d-flex justify-content-center">Price:${product.price}$</h5>
+                        <input class="quantity" type="number" min="1" max="10">
+                        <button  id="addProductToCart" type="button" class="btn btn-success rounded">Add to Cart</button>
+                    </div>
+                
+                
                 </div>
+                </div
             `;
             this.detailsDiv.innerHTML = output;
             }
@@ -51,14 +59,14 @@ class  UI {
                 let output = '';
                 products.forEach((product) => {
                     output = `
-                    <table id="table-admin">
+                    <table id="table-admin" class="table">
                     <tbody> 
-                    <tr>
+                    <tr scope="row">
                         <td><img src="${product.image}" class="admin-card-img"</td>
                         <td>${product.title}</td>
-                        <td>${product.price}</td>
+                        <td>${product.price}$</td>
                         <td>10</td>
-                        <td><button id=${product.id} type="button" class="card-button delete">Remove</button></td>
+                        <td><button id=${product.id} type="button" class="card-button delete"><i class="far fa-trash-alt"></i></button></td>
                     </tr>
                 </tbody>   
                 </table> 
